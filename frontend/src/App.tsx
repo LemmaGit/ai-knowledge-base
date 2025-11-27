19c2cefc8aadb697445a50d61dd42518dad3797f
LemmaGit <getiyelemma07@gmail.com>
2025-11-27 11:12:44 +0300

    installed daisyUI and lucid icons and ready to start the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="flex justify-center mt-4"></div>;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggelTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    if (!isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+  return (
+    <div className="h-dvh flex justify-center items-center gap-4 bg-primaryy dark:bg-dark transition-colors duration-500 p-4">
+      {/* <button
+        onClick={toggelTheme}
+        type="button"
+        className="h-fit border  py-2 px-4 rounded-lg text-sm cursor-pointer hover:bg-orange-600 bg-orange-500 text-dark dark:text-primaryy"
+      >
+        Change Theme
+      </button> */}
+      <button
+        onClick={toggelTheme}
+        type="button"
+        className={`btn btn-lg ${isDarkMode ? "btn-primary" : "btn-secondary"}`}
+      >
+        Change Theme
+      </button>
+      <button
+        className={`btn btn-lg ${isDarkMode ? "btn-primary" : "btn-secondary"}`}
+        type="button"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2.5"
+          stroke="currentColor"
+          className="size-[1.2em]"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+          />
+        </svg>
+        Like
+      </button>
+    </div>
+  );
 }
 
 export default App;
